d5f59902c3e3693d6843c16a938f2e1ab3b4c1b9
楊竑昕 <jk195417@gmail.com>
2017-12-22 00:38:21 +0800

    vue lottery

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,1 +1,44 @@
+import Vue from 'vue/dist/vue.js'
 
+const lottery = new Vue({
+  el: '#lottery',
+  data: {
+    min: 1,
+    max: 10,
+    numbers: [],
+    notification: ''
+  },
+  methods: {
+    draw: () => {
+      self = lottery
+      const luckyNumber = self.candidateNumbers[Math.floor(Math.random() * self.candidateNumbers.length)]
+      if (luckyNumber === undefined) {
+        alert('所有號碼都被抽完了!')
+      } else {
+        self.numbers.push(luckyNumber)
+        self.notification = `${luckyNumber}`
+        return luckyNumber
+      }
+    },
+    cancel: (number) => {
+      const index = self.numbers.indexOf(number)
+      return self.numbers.splice(index, 1)
+    },
+    clearNotification: () => {
+      self.notification = ''
+    }
+  },
+  computed: {
+    candidateNumbers: () => {
+      self = lottery
+      let ary = []
+      for (let i = self.min; i <= self.max; i++) {
+        if (!self.numbers.includes(i)) {
+          ary.push(i)
+        }
+      }
+      return ary
+    }
+  }
+});
+window.lottery = lottery
