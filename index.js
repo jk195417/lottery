9540c6e641e07810d42055e90a5df33b65337e63
楊竑昕 <jk195417@gmail.com>
2017-12-21 20:38:24 +0800

    use bulma replace bootstrap

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 // parcel enter point
+
 // import js here
-// BUG: parcel load bootstrap faild, use cdn to temp fix, see: https://github.com/parcel-bundler/parcel/issues/333
-// import 'jquery/dist/jquery.js'; // jquery
-// import 'popper.js/dist/umd/popper.js'; // popper
-// import 'bootstrap/dist/js/bootstrap.js'; // bootstrap
 import './assets/js/application.js';
 
 // import css here
+import 'bulma/bulma';
 import './assets/css/application.scss';
+
 // import img here
