865518c763d8820bfb2454968fe2f724156a24ef
張子晏 <try_love_tom@icloud.com>
2017-12-22 18:53:39 +0800

    Remove Lottery Component Local Registration lottery

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,61 +1,9 @@
 import Vue from 'vue/dist/vue.js'
+import lottery from './lottery'
 
-const lottery = new Vue({
-  el: '#lottery',
-  data: {
-    min: 1,
-    max: 10,
-    numbers: [],
-    notification: '',
-    drawing: false,
-    noticing: false
-  },
-  methods: {
-    draw: () => {
-      self = lottery
-      const luckyNumber = self.candidateNumbers[Math.floor(Math.random() * self.candidateNumbers.length)]
-      if (luckyNumber === undefined) {
-        alert('所有號碼都被抽完了!')
-      } else {
-        self.drawing = true
-        setTimeout(() => {
-          self.drawing = false
-          self.numbers.push(luckyNumber)
-          self.numbers = self.ascNumbers
-          self.notification = `${luckyNumber}`
-          self.noticing = true
-          setTimeout(() => {
-            self.noticing = false
-          }, 2000)
-        }, 2000)
-        return luckyNumber
-      }
-    },
-    cancel: (number) => {
-      const index = self.numbers.indexOf(number)
-      return self.numbers.splice(index, 1)
-    },
-    clearNotification: () => {
-      self.notification = ''
-    }
-  },
-  computed: {
-    ascNumbers: () => {
-      self = lottery
-      return self.numbers.sort((a, b) => {
-        return a - b
-      })
-    },
-    candidateNumbers: () => {
-      self = lottery
-      let ary = []
-      for (let i = self.min; i <= self.max; i++) {
-        if (!self.numbers.includes(i)) {
-          ary.push(i)
-        }
-      }
-      return ary
-    }
+const vm = new Vue({
+  el: '#app',
+  components: {
+    'lottery': lottery
   }
-});
-window.lottery = lottery
+})
