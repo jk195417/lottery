0d2dad22d40919d3899dcafc52ee0683580a0136
楊竑昕 <jk195417@gmail.com>
2019-12-29 22:54:45 +0800

    add plugins vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'eslint:recommended', 'plugin:vue/essential', '@vue/standard'
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/essential', '@vue/standard'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
-  }
-}
+  },
+  plugins: ['vue']
+};
